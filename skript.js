90d1b15a40e7f202f24d14b8c2bca2f6b9acad4a
overjjang <overjjang99@gmail.com>
2024-10-23 14:19:55 +0900

    검색되는 학교 이름 있을 시 선택할 수 있게 변경, getMealInfo함수에 인자를 넣도록 변경

--- skript.js
+++ skript.js
@@ -5,6 +5,16 @@ const apiurl = 'https://gubsicmenu.overjjang99.workers.dev/api/todos';
 let isReturing = "";
 let date = new Date().toISOString().slice(0, 10).replace(/-/g, '');
 
+const container = document.getElementById('container');
+const title = document.getElementById('title');
+const menu = document.getElementById('menuList');
+
+const otherSchool = document.getElementById('otherschoolcheak');
+const dateinput = document.getElementById('dateinput').value.replace(/-/g, '');
+
+const schoollist = document.getElementById('schoolList');
+
+
 function setDate(){
     document.getElementById('dateinput').value = new Date().toISOString().slice(0, 10);
 }
@@ -13,32 +23,43 @@ setDate()
 
 function schoolNameInput(fixedSchoolName = '') {
     const input = document.getElementById('schoolnameinput').value;
-    const otherSchool = document.getElementById('otherschoolcheak');
-    const container = document.getElementById('container');
-    const title = document.getElementById('title');
-    const dateinput = document.getElementById('dateinput').value.replace(/-/g, '');
     console.log(dateinput);
     date = dateinput ? dateinput : date;
     title.innerHTML = `오늘의 급식은?`;
     container.style.visibility = `hidden`;
     otherSchool.style.visibility = `hidden`;
+    otherSchool.style.display = `none`;
     schoolname = fixedSchoolName ? fixedSchoolName : (input ? input : schoolname);
     main()
 }
 
 async function getSchoolInfo(schoolName) {
     let apiUrl = `https://gubsicmenu.overjjang99.workers.dev/api?mode=name&schoolName=${schoolName}`;
-    const container = document.getElementById('container');
-    const title = document.getElementById('title');
-    const menu = document.getElementById('menuList');
+    listLabel = document.getElementById('listLabel');
+    const schoolSelect = document.getElementById('schoolSelect');
     try {
+
+        //@todo 중복된 학교명 해결법 모색
         let response = await fetch(apiUrl);
         let data = await response.json();
         console.log(data);
         if(data.schoolInfo) {
-            atptCode = data.schoolInfo[1].row[0].ATPT_OFCDC_SC_CODE;
-            sdSchulCode = data.schoolInfo[1].row[0].SD_SCHUL_CODE;
-            isReturing = data.schoolInfo[0].head[1].RESULT.CODE;
+            if (data.schoolInfo[0].head[0].list_total_count === 1) {
+                atptCode = data.schoolInfo[1].row[0].ATPT_OFCDC_SC_CODE;
+                sdSchulCode = data.schoolInfo[1].row[0].SD_SCHUL_CODE;
+                isReturing = data.schoolInfo[0].head[1].RESULT.CODE;
+            }
+            else{
+                listLabel.innerHTML = `학교 이름이 ${schoolName}인 학교가 ${data.schoolInfo[0].head[0].list_total_count}개 입니다. 선택해주세요.`;
+                schoolSelect.innerHTML = "";
+                schoolSelect.innerHTML += `<option value="">학교를 선택해주세요</option>`;
+                for(let i = 0; i < data.schoolInfo[0].head[0].list_total_count; i++) {
+                    schoolSelect.innerHTML += `<option value="${data.schoolInfo[1].row[i].ATPT_OFCDC_SC_CODE},${data.schoolInfo[1].row[i].SD_SCHUL_CODE}">${data.schoolInfo[1].row[i].SCHUL_NM}(${data.schoolInfo[1].row[i].ATPT_OFCDC_SC_NM})</option>`;
+                }
+                schoollist.style.visibility = `visible`;
+                schoollist.style.display = `inline-block`;
+                isReturing="needSelect";
+            }
         }
         else isReturing = data.RESULT.CODE;
         console.log(atptCode, sdSchulCode);
@@ -49,13 +70,9 @@ async function getSchoolInfo(schoolName) {
         menu.innerHTML = "";
     }
 }
-function getMealInfo() {
-    let apiUrl = `https://gubsicmenu.overjjang99.workers.dev/api?mode=menu&atptCode=${atptCode}&schoolCode=${sdSchulCode}&date=${date}`;
-    const container = document.getElementById('container');
-    const lunch = document.getElementById('menuList');
-    const title = document.getElementById('title');
-    const menu = document.getElementById('menuList');
-    const otherSchool = document.getElementById('otherschoolcheak');
+function getMealInfo(inAtptCode, inSdSchulCode) {
+    console.log(inAtptCode, inSdSchulCode);
+    let apiUrl = `https://gubsicmenu.overjjang99.workers.dev/api?mode=menu&atptCode=${inAtptCode}&schoolCode=${inSdSchulCode}&date=${date}`;
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
@@ -72,13 +89,19 @@ function getMealInfo() {
                 menu.innerHTML += `<h3 class="fs-4 fw-bold " align="center">${data.mealServiceDietInfo[1].row[i].MMEAL_SC_NM}</h3>${data.mealServiceDietInfo[1].row[i].DDISH_NM}`;
             }
             otherSchool.style.visibility = `visible`;
+            otherSchool.style.display = `inline-block`;
             container.style.visibility = `visible`;
+            container.style.display = `inline-block`;
+            schoollist.style.visibility = `hidden`;
+            schoollist.style.display = `none`;
             title.classList.remove('error');
         })
         .catch(error => {
             title.classList.add('error');
             console.log(error);
+            console.log(apiUrl);
             otherSchool.style.visibility = `visible`;
+            otherSchool.style.display = `inline-block`;
             title.innerHTML = `급식 정보가 없습니다.`;
             menu.innerHTML = "";
         });
@@ -88,19 +111,22 @@ async function main() {
     await getSchoolInfo(schoolname);
     console.log(isReturing);
     if(isReturing === "INFO-200") {
-        const container = document.getElementById('container');
-        const title = document.getElementById('title');
-        const menu = document.getElementById('menuList');
-        const otherSchool = document.getElementById('otherschoolcheak');
         otherSchool.style.visibility = `visible`;
+        otherSchool.style.display = `inline-block`;
         console.log("school not found");
         container.style.visibility = `hidden`;
         title.innerHTML = `해당 학교를 찾을 수 없습니다`;
         menu.innerHTML = "";
         title.classList.add('error');
     }
+    else if(isReturing === "needSelect") {
+        console.log("need select");
+        title.innerHTML = `중복되는 학교명이 있습니다. 선택해주세요.`;
+        container.style.visibility = `hidden`;
+        container.style.display = `none`;
+    }
     else {
-        getMealInfo();
+        getMealInfo(atptCode, sdSchulCode);
     }
 }
 
